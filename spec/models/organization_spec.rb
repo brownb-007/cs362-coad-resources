b46a4fdb2ad018843006ec49eafd541dafca67af
Brayden Brown <braydenbrown@gmail.com>
2021-01-12 15:26:28 -0800

    Organization: Instantiate an organization with the use of let, add space for more attribute tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
-  organization = Organization.new
+  let(:organization) { Organization.new }
 
   it "instantiate an organization object" do
     organization
@@ -12,6 +12,8 @@ RSpec.describe Organization, type: :model do
     it "has an email" do
       expect(organization).to respond_to(:email)
     end
+
+
     
   end
   

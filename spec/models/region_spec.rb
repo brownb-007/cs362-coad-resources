bbf16d3b592d17c6202319cb4c978f9952c6406e
Brayden Brown <braydenbrown@gmail.com>
2021-01-14 11:19:08 -0800

    Region: Chack if it has a name and if it exists

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+  it "exists" do
+    Region.new
+  end
+
+  it "has a name" do
+    region = Region.new
+    expect(region).to respond_to(:name)
+  end
+  
+
+
 end

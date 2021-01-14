89847b7fadee35a045161cfb123a1a8244f56b46
Brayden Brown <braydenbrown@gmail.com>
2021-01-14 11:55:27 -0800

    Region: test if exists, has a name, and validate the presence of name

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+  let(:region) { Region.new }
+
   it "exists" do
-    Region.new
+    region
   end
 
   it "has a name" do
-    region = Region.new
     expect(region).to respond_to(:name)
   end
   
+  describe "validations" do
+    it "validates the presence of name" do
+      expect(region).to validate_presence_of(:name)
+    end
 
+  end
 
 end

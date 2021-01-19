772f55a2309e46f07431aa27e9cf4f020f28659d
Brayden Brown <braydenbrown@gmail.com>
2021-01-18 19:37:54 -0800

    Region: take out name validation test as it is already tested earlier in the spec

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -13,10 +13,6 @@ RSpec.describe Region, type: :model do
   end
   
   describe "validations" do
-    it "validates the presence of name" do
-      expect(region).to validate_presence_of(:name)
-    end
-
     it "validates the length of name" do
       expect(region).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
     end

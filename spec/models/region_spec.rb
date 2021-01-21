057cc41837f93b112c53da7b89f1beb5bb90c7dd
Brayden Brown <braydenbrown@gmail.com>
2021-01-21 11:07:51 -0800

    Region: put tests in describe blocks, Resource_category: test attributes

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -3,11 +3,16 @@ require 'rails_helper'
 RSpec.describe Region, type: :model do
   let(:region) { Region.new }
 
-  it { is_expected.to respond_to(:name) }
-
-  it { is_expected.to have_many(:tickets) } 
-
-  describe "validations" do
+  describe "Attributes" do
+    it { is_expected.to respond_to(:name) }
+  end
+  
+  describe "Associations" do
+    it { is_expected.to respond_to(:name) }
+    it { is_expected.to have_many(:tickets) } 
+  end
+   
+  describe "Validations" do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
     it { is_expected.to validate_uniqueness_of(:name).case_insensitive() }

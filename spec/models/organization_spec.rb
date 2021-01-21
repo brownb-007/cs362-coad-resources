d31a3afc67f22246022cc0df65fe83e7e3ac27c7
Brayden Brown <braydenbrown@gmail.com>
2021-01-21 10:13:20 -0800

    Organization: not allow invalid value of email

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -35,6 +35,7 @@ RSpec.describe Organization, type: :model do
     it { is_expected.to validate_presence_of(:secondary_phone) }
     it { is_expected.to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create) }
     it { is_expected.to allow_value("fake@mail.com").for(:email) }
+    it { is_expected.to_not allow_value("mail.com", "fake").for(:email) }
   end
   
 

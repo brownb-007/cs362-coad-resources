4e11bc22402853cb5d2bcbad6121c1678e686f54
Brayden Brown <braydenbrown@gmail.com>
2021-01-26 11:41:10 -0800

    User spec outline

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+  describe "Attributes" do
+    
+  end
+
+  describe "Associations" do
+    
+  end
+
+  describe "Validations" do
+    
+  end
+
 end

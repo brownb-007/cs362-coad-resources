db95720f669aee7c11a82e9c98639ca6ca937592
Brayden Brown <braydenbrown@gmail.com>
2021-01-26 11:30:29 -0800

    Resource_category: add validation tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -14,7 +14,11 @@ RSpec.describe ResourceCategory, type: :model do
   end
 
   describe "Validations" do
-    
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive() }
   end
+
+  
 
 end

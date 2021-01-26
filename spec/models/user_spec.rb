56d6b136bc8d79133896df3465cba5e0e487f9a8
Brayden Brown <braydenbrown@gmail.com>
2021-01-26 11:41:30 -0800

    User spec outline

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  describe "Attributes" do
+    
+  end
+
+  describe "Associations" do
+    
+  end
+
+  describe "Validations" do
+    
+  end
+
 end

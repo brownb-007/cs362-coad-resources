76170c3f0a490b6af5c4837549d74629ddd8f30a
Brayden Brown <braydenbrown@gmail.com>
2021-01-28 10:55:05 -0800

    Ticket add validations still have to test the phone number but unsure how

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -15,11 +15,14 @@ RSpec.describe Ticket, type: :model do
   describe "Associations" do
     it { is_expected.to belong_to(:region) }
     it { is_expected.to belong_to(:resource_category) }
-    it { is_expected.to belong_to(:organization) }
+    #TODO optional? it { is_expected.to belong_to(:organization) }
   end
 
   describe "Validations" do
-    
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:phone) }
+    it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
+    it { is_expected.to validate_length_of(:description).is_at_most(1020).on(:create) }
   end
 
 end

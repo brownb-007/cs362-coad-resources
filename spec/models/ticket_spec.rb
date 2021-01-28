36528bb7e9a7aaa9ef5aa151f3c480b521ba3fcf
Brayden Brown <braydenbrown@gmail.com>
2021-01-28 10:34:37 -0800

    Create a ticket and test attibutes

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+  let (:ticket) { Ticket.new }
+
   describe "Attributes" do
-    
+    it { is_expected.to respond_to(:name) }
+    it { is_expected.to respond_to(:description) }
+    it { is_expected.to respond_to(:phone) }
+    it { is_expected.to respond_to(:closed) }
+    it { is_expected.to respond_to(:closed_at) }
   end
 
   describe "Associations" do

107d0662a3b1d625763c3179d9a9cf99fd892faf
Brayden Brown <braydenbrown@gmail.com>
2021-02-01 20:14:59 -0800

    spec/factories/resource_categories.rb
    Resource_categories: Create a factory for lazy instantiation

---
+++ spec/factories/resource_categories.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :resource_category do 
+    sequence(:name) { |n| "resource_category #{n}"}
+    organization
+
+    trait :active do
+      actvie { true }
+    end
+
+    trait :inactive do 
+      active { false }
+    end
+  end
+end

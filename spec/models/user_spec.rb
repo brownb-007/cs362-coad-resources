272efe0e8e12dcd279a086487954e6f31de83eb8
Brayden Brown <braydenbrown@gmail.com>
2021-02-07 13:26:05 -0800

    User: write tests for validations, associations, attributes, and write tests for methods and what role a user is assigned

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { User.new }
 
   describe "Attributes" do
-    
+    it { is_expected.to respond_to(:email) }
+    it { is_expected.to respond_to(:role) }
   end
 
   describe "Associations" do
-    
+    it { is_expected.to belong_to(:organization).optional }
   end
 
   describe "Validations" do
-    
+    it { is_expected.to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create) }
+    it { is_expected.to allow_value("fake@mail.com").for(:email) }
+    it { is_expected.to_not allow_value("mail.com", "fake").for(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive() }
+    it { is_expected.to validate_presence_of(:email) }
   end
 
+  describe "Methods" do
+    describe "@to_s" do 
+      it "has a string representation of email" do
+        user_email = User.new(email: "fake@mail.com")
+        expect(user_email.to_s).to eq("fake@mail.com") 
+      end
+    end
+
+    describe "@set_default_role" do
+      it "assigns a default role to a new user" do
+        user.role = :organization
+        expect(user.admin?).to eq(false)
+      end
+      
+      it "assigns a admin role to a user" do
+        user.role = :admin
+        expect(user.admin?).to eq(true)
+      end   
+    end
+  end
 end

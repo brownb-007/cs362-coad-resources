d0c98d0d5c8469b48661168dda1a7e3c4b5d73c8
Brayden Brown <braydenbrown@gmail.com>
2021-02-06 19:58:17 -0800

    Ticket: Create several instances with different states, create tests for methods and comment out phoney plausible test

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-  let (:ticket) { Ticket.new }
+  let(:ticket) { create(:ticket) }
+  let(:closed_ticket) { create(:ticket, :closed_ticket) }
+  let(:open_ticket) { create(:ticket, :open_ticket) } 
+  let(:ticket_has_open_organization) { create(:ticket, :open_ticket, :has_organization) }
+  let(:ticket_has_closed_organization) { create(:ticket, :closed_ticket, :has_organization) }
 
   describe "Attributes" do
     it { is_expected.to respond_to(:name) }
@@ -24,9 +28,45 @@ RSpec.describe Ticket, type: :model do
     it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
     it { is_expected.to validate_length_of(:description).is_at_most(1020).on(:create) }
 
-    it "has a valid phone number" do
+    # it "has a valid phone number" do
+    #   let(:valid_ticket_number) { create(:ticket, phone = "+311012341234") }
+    #   let(:invalid_ticket_number) { create(:ticket, phone = "12345") }
+    #   expect(:valid_ticket_number.save!).to 
+    #   expect(:invalid_ticket_number.save!).to 
+    # end
+  end
+
+  describe "Methods" do
+
+    describe "@to_s" do 
+      it 'has a string representation of ticket' do
+        temp_tick = build(:ticket, id: '98765')
+        expect(temp_tick.to_s).to eq('Ticket ' + '98765') 
+      end
+    end
+
+    describe "@opened?" do
+      it "has a status of open and not closed" do
+        expect(Ticket.open).to include(open_ticket)
+        expect(Ticket.open).to_not include(closed_ticket)
+      end
+    end
+    
+    describe "@captured" do
+      it "is not captured aaas a defualt" do
+        expect(ticket).to_not be_captured
+      end
       
+      it "is captured if it has an organization" do
+        ticket.organization = Organization.new
+        expect(ticket).to be_captured
+      end
     end
   end
+
+  describe "Scopes" do
+    
+  end
+  
 
 end

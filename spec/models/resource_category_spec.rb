8457f5bc1d647021ae66fb4aaf303c84cb873e55
Brayden Brown <braydenbrown@gmail.com>
2021-02-06 17:43:06 -0800

    Resource_category: create tests for all of the statuses.

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
   let(:resource_category) { build(:resource_category) }
+  let(:resource_category_active) { build(:resource_category, :active) }
+  let(:resource_category_inactive) { build(:resource_category, :inactive) }
 
   describe "Attributes" do
     it { is_expected.to respond_to(:name) }
@@ -21,7 +23,7 @@ RSpec.describe ResourceCategory, type: :model do
 
   describe "Methods" do
 
-    it "#to_s, has a string representation of name" do
+    it "@to_s, has a string representation of name" do
       resource_category = build(:resource_category, name: "Fake")
       expect(resource_category.to_s).to  eq("Fake")
     end
@@ -30,8 +32,20 @@ RSpec.describe ResourceCategory, type: :model do
       expect(ResourceCategory.unspecified.name).to eq('Unspecified')
     end
 
-    
+    describe "Activity statuses" do 
+      it "@activate has a status of active" do
+        expect(resource_category.active).to be_truthy
+      end
 
+      it "@deactivate has a deactivated activity status" do
+        expect(resource_category.deactivate).to be_truthy
+      end
 
+      it "@inactive detemines if the resource category is active or inactive" do 
+        expect(resource_category.inactive?).to be_falsey
+      end
+    end
   end
+
+  
 end

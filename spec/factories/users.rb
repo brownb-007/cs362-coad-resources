da926cc7536a93e28679733cd9a9e7c8d8678a2d
Brayden Brown <braydenbrown@gmail.com>
2021-02-14 12:43:06 -0800

    Users: create a user factory for easier testing

---
+++ spec/factories/users.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:email) { |n| "person#{n}@example.com"}
+    password { "Fake" }
+
+    trait :admin do
+      role { 'admin' }
+    end
+
+    trait :mem_of_org do 
+      role { :organization }
+    end
+
+    after :create do |u|
+      u.confirm
+    end
+  end
+end

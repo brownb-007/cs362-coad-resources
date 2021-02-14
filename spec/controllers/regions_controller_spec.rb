6ae031b21343df418b5c5f9c2e986b0d8591f5e7
Brayden Brown <braydenbrown@gmail.com>
2021-02-14 13:23:47 -0800

    Regions: add a test case for an organization user to redirect them to the dashboard when using paths

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -46,6 +46,30 @@ RSpec.describe RegionsController, type: :controller do
     end
   end
 
+  context "an organization user" do
+    before do
+      organization_user = build(:user, :mem_of_org)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(organization_user)
+      allow(controller).to receive(:current_user).and_return(organization_user)
+    end
+
+    describe "dashboard redirection" do
+      it "redirects user to the dashboard" do
+        get :index
+        expect(response).to  redirect_to(dashboard_url)
+        get :show, params: {id: 'fake'}
+        expect(response).to  redirect_to(dashboard_url)
+        get :new
+        expect(response).to  redirect_to(dashboard_url)
+        post :create
+        expect(response).to  redirect_to(dashboard_url)
+        put :update, params: {id: 'fake'}
+        expect(response).to  redirect_to(dashboard_url)
+        delete :destroy, params: {id: 'fake'}
+        expect(response).to  redirect_to(dashboard_url)
+      end
+    end
+  end
   
   
 end

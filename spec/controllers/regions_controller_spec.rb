d363abe9a26dd4d7791e0c58fabc5deee796c0eb
Brayden Brown <braydenbrown@gmail.com>
2021-02-14 13:55:02 -0800

    Regions: add edit to all tests, create a context for an admin user to use all of the routes

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -24,6 +24,13 @@ RSpec.describe RegionsController, type: :controller do
       end 
     end
 
+    describe "#edit" do
+      it "redirects to the sign_in screen" do
+        get :edit, params: {id: 'fake'}
+        expect(response).to  redirect_to(new_user_session_url)
+      end 
+    end
+
     describe "#create" do
       it "redirects to the sign_in screen" do
         post :create
@@ -61,6 +68,8 @@ RSpec.describe RegionsController, type: :controller do
         expect(response).to  redirect_to(dashboard_url)
         get :new
         expect(response).to  redirect_to(dashboard_url)
+        get :edit, params: {id: 'fake'}
+        expect(response).to  redirect_to(dashboard_url)
         post :create
         expect(response).to  redirect_to(dashboard_url)
         put :update, params: {id: 'fake'}
@@ -70,6 +79,55 @@ RSpec.describe RegionsController, type: :controller do
       end
     end
   end
-  
-  
+
+  context "an admin user" do
+    let(:region) {create(:region)}
+    before do
+      admin_user = create(:user, :admin)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(admin_user)
+      allow(controller).to receive(:current_user).and_return(admin_user)
+    end
+
+    describe "#index" do
+      it "gets the index" do
+        get :index
+        expect(response).to  be_successful
+      end 
+    end
+
+    describe "#show" do
+      it "gets show" do
+        get :show, params: {id: region.id}
+        expect(response).to  be_successful
+      end 
+    end
+
+    describe "#new" do
+      it "gets new" do
+        get :new
+        expect(response).to  be_successful
+      end 
+    end
+
+    describe "#create" do
+      it "posts a created region" do
+        post :create, params: {region: attributes_for(:region)}
+        expect(response).to  redirect_to(regions_path)
+      end 
+    end
+
+    describe "#update" do
+      it "puts an updated region" do
+        put :update, params: {id: region.id, region: attributes_for(:region)}
+        expect(response).to  redirect_to(region_path(region))
+      end 
+    end
+
+    describe "#destroy" do
+      it "deletes a region" do
+        delete :destroy, params: {id: region.id}
+        expect(response).to  redirect_to(regions_path)
+      end 
+    end
+  end
 end

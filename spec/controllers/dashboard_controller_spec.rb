f165ac40452e07fe42a2b546a657e5932f3364be
Brayden Brown <braydenbrown@gmail.com>
2021-02-14 16:43:05 -0800

    Dashboard: Create test for unauthenticated users, organization users, and admin users to get the index

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
+  context "unauthenticated users" do
+    describe "#index" do
+      it "gets the index of the dashboard" do
+        get :index
+        expect(response).to  redirect_to(new_user_session_url)
+      end
+    end
+  end
 
+  context "an organization user" do
+    before do
+      organization_user = build(:user, :mem_of_org)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(organization_user)
+      allow(controller).to receive(:current_user).and_return(organization_user)
+    end
+
+    describe "#index" do
+      it "get index" do
+        get :index
+        expect(response).to  be_successful
+      end 
+    end
+  end
+
+  context "an admin user" do
+    before do
+      admin_user = create(:user, :admin)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(admin_user)
+      allow(controller).to receive(:current_user).and_return(admin_user)
+    end
+
+    describe "#index" do
+      it "get index" do
+        get :index
+        expect(response).to  be_successful
+      end 
+    end
+  end
+  
 end

a67b638288694be6c4185d8dfe5382b714238b35
Brayden Brown <braydenbrown@gmail.com>
2021-02-28 15:10:13 -0800

    Region Feature: Add a system test for an admin creating a region

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
-
+  let(:admin) { admin = create(:user, :admin) }
+  it "Admin creates a region" do
+    log_in_as(admin)
+    click_link "Regions"
+    click_on "Add Region"
+    expect(page).to have_content("Add a region")
+    fill_in "Name",	with: "Fake Region"
+    click_button "Add Region"
+    expect(page).to have_content("Region successfully created.")
+  end
 end

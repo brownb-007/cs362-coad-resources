66830dfce2d272635ba9fb66be06798340f7dfd6
Brayden Brown <braydenbrown@gmail.com>
2021-02-28 15:54:15 -0800

    Region Features: change delete system test

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Deleting a Region', type: :feature do
     log_in_as(admin)
     visit region_path(region)
     click_on "Delete"
-    expect(page.body).to have_content("Regions")
+    expect(page.body).to have_text(region.name)
     expect(page.body).to have_content("Region #{region} was deleted. Associated tickets now belong to the 'Unspecified' region.")
   end
 end

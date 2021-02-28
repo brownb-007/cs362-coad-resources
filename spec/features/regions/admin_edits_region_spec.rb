d715dd0684fc223d6b07f024d61326fb16943bdc
Brayden Brown <braydenbrown@gmail.com>
2021-02-28 15:54:56 -0800

    Region Features: Add a system test for an admin editing a region

---
+++ spec/features/regions/admin_edits_region_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Editing Region', type: :feature do
+  let(:admin) { admin = create(:user, :admin) }
+  
+  specify "Admin edits a region" do
+    region = Region.create(name: "fake region")
+    log_in_as(admin)
+    visit region_path(region)
+    click_on "Edit Region"
+    expect(page.body).to have_text(region.name)
+    fill_in "Name",	with: "Fake Edited Region"
+    click_button "Save Changes"
+    expect(page.body).to have_content("Region successfully updated")
+  end
+end

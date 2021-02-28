fd4a506e96b47bca0cab708f5fe83dad89161c38
Brayden Brown <braydenbrown@gmail.com>
2021-02-28 15:39:48 -0800

    Region Feature: Add a system test for deleting a region behavior

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
-
+  let(:admin) { admin = create(:user, :admin) }
+  
+  it "Admin deletes a region" do
+    region = Region.create(name: "fake region")
+    log_in_as(admin)
+    visit region_path(region)
+    click_on "Delete"
+    expect(page.body).to have_content("Regions")
+    expect(page.body).to have_content("Region #{region} was deleted. Associated tickets now belong to the 'Unspecified' region.")
+  end
 end

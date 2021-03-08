646878dd55559848c78e143379210ee522ce3884
Brayden Brown <braydenbrown@gmail.com>
2021-03-07 17:09:40 -0800

    Organization: Create a system test for rejecting a submitted organization

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
-
+  let(:admin) { admin = create(:user, :admin) }
+  let(:organization) { organization = build(:organization, :submitted)}
+  describe "Admin interacts with a submitted organization" do
+    specify "Admin rejects a submitted organization" do
+      log_in_as(admin)
+      visit organizations_path
+      click_on 'Pending'
+      visit organizations_path(organization)
+      click_on 'Reject'
+      expect(page).to have_text('Rejected')
+    end
+  end
 end

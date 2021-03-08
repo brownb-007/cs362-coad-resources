3e1eee5692ea97e56d33faeceaf8772be0502761
Brayden Brown <braydenbrown@gmail.com>
2021-03-07 17:05:53 -0800

    Organization: Create system test for approving an organization and not being able to approve an already approved organization

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
+  let(:admin) { admin = create(:user, :admin) }
+  let(:organization) { organization = build(:organization, :submitted)}
+  describe "Admin interacts with a submitted organization" do
+    specify "Admin approves a submitted organization" do
+      log_in_as(admin)
+      visit organizations_path(organization)
+      click_on 'Approve'
+      expect(page).to have_text('Approved')
+    end
 
+    specify "Admin can't re-approve approved organizations" do
+      log_in_as(admin)
+      visit organizations_path
+      click_on 'Approved'
+      visit organization
+      expect(page).not_to have_link('Approved')
+    end
+  end
 end

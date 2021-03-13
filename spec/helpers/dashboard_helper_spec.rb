a8a19c179bebf524bf242b706ed3c480ab3078f8
Brayden Brown <braydenbrown@gmail.com>
2021-03-13 13:08:44 -0800

    Dashboard helper: implement the use of double() rather than using build or create

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -11,23 +11,34 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe DashboardHelper, type: :helper do
+  before do
+    @user = double()
+  end
+
   describe "#dashboard_for" do
     it "takes admin to admin dashboard" do
-      expect(helper.dashboard_for(build(:user, :admin))).to eq("admin_dashboard") 
+			allow(@user).to receive(:admin?) { true }
+      expect(helper.dashboard_for(@user)).to eq("admin_dashboard") 
     end
 
     it "takes submitted organization to submitted dashboard" do
-      org = build(:organization)
-      expect(helper.dashboard_for(build(:user, organization: org))).to eq("organization_submitted_dashboard")
+      allow(@user).to receive(:admin?) { false }
+      allow(@user).to receive_message_chain(:organization, :submitted?) { true }
+      expect(helper.dashboard_for(@user)).to eq("organization_submitted_dashboard")
     end
     
     it "takes approved organization to approved dashboard" do
-      approved_org = build(:organization, :approved)
-      expect(helper.dashboard_for(build(:user, :mem_of_org, organization: approved_org))).to eq("organization_approved_dashboard")
+      allow(@user).to receive(:admin?) { false }
+      allow(@user).to receive_message_chain(:organization, :submitted?) { false }
+      allow(@user).to receive_message_chain(:organization, :approved?) { true }
+      expect(helper.dashboard_for(@user)).to eq("organization_approved_dashboard")
     end
     
     it "takes unauthorized user to the default dashboard" do
-      expect(helper.dashboard_for(build(:user))).to eq("create_application_dashboard") 
+      allow(@user).to receive(:admin?) { false }
+      allow(@user).to receive_message_chain(:organization, :submitted?) { false }
+      allow(@user).to receive_message_chain(:organization, :approved?) { false }
+      expect(helper.dashboard_for(@user)).to eq("create_application_dashboard") 
     end
   end
 end

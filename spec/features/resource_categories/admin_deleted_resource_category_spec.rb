090156d7d02e2e03a8f80bf6a2cf6b83d14931fc
Brayden Brown <braydenbrown@gmail.com>
2021-03-13 19:17:44 -0800

    Resource_category: create a system test for deleting a resource category with a happy path

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
+  let(:admin) { admin = create(:user, :admin) }
+  let(:resource_category) { resource_category = create(:resource_category) }
 
+  specify "Admin successfully deletes a resource category" do
+    log_in_as(admin)
+    visit resource_category_path(resource_category)
+    click_on "Delete"
+    expect(page).to have_text("Category #{resource_category} was deleted")
+  end
 end

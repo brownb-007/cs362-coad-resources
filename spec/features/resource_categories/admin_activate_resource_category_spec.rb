4c8103c5e8056117ba6b2a44c811140b25f611fd
Brayden Brown <braydenbrown@gmail.com>
2021-03-13 19:31:25 -0800

    Resource_category: add system test for an admin activating and deactivating a resource_category

--- spec/features/resource_categories/admin_activate_resource_category_spec.rb
+++ spec/features/resource_categories/admin_activate_resource_category_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'Deactivating a Resource Category', type: :feature do
+RSpec.describe 'Activating a Resource Category', type: :feature do
   let(:admin) { admin = create(:user, :admin) }
   let(:resource_category) { resource_category = create(:resource_category) }
 
-  specify "Admin successfully deactivates a resource category" do
+  specify "Admin successfully activates a resource category" do
     log_in_as(admin)
     visit resource_category_path(resource_category)
     click_on "Deactivate"
     expect(page.body).to have_text("Category deactivated")
+    click_on "Activate"
+    expect(page.body).to have_text("Category activated")
   end
 end

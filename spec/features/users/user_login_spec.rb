ed0e77657bfe00d811a1c3c59bb566e4b9cd012c
Brayden Brown <braydenbrown@gmail.com>
2021-03-14 17:25:22 -0700

    User: create a system test for simulating a user logging in

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
-
+  let(:user) { create(:user) }
+  
+  specify "Log in as a user" do
+    visit login_path
+    fill_in "user_email", with: user.email
+    fill_in "user_password", with: user.password
+    click_on "Sign in"
+    expect(page).to have_text("Signed in successfully")
+  end
 end

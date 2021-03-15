ad7d1752daa4fb770d35dc023a714eae5b6c5b8b
Brayden Brown <braydenbrown@gmail.com>
2021-03-14 17:23:58 -0700

    Ticket: system test for a user creating and submitting a ticket

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
-
+  specify "Person creates a ticket" do
+    region = Region.create(name: "Fake region")
+    resource_category = ResourceCategory.create(name: "Fake category")
+    visit root_path
+    click_on "Get Help"
+    fill_in "Full Name", with: "Fake"
+    fill_in "Phone Number", with: "1234561234"
+    select region.name, from: "Region"
+    select resource_category.name, from: "Resource Category"
+	  fill_in "Description", with: "Fake description"
+	  click_on "Send this help request"
+    visit ticket_submitted_path
+    expect(page).to have_text("Ticket Submitted") 
+  end
 end

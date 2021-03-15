09c32613f8dd41590e9927cbbec60e724fd9e863
Brayden Brown <braydenbrown@gmail.com>
2021-03-14 17:16:20 -0700

    Organization: create system test for updating an organization

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
-  
+  let(:organization) { create(:organization, :approved) }
+  let(:user) { create(:user, :mem_of_org, organization: organization) }
+
+  describe "Organization edits their own organization successfully" do
+    specify "Edit organization" do
+      log_in_as(user)
+      create(:user, :admin)
+      expect(page).to have_text("Edit Organization")
+      click_on "Edit Organization"
+      fill_in "Name", with: "Fake Org"
+      fill_in "Phone", with: "555-555-555"
+      fill_in "Email", with: "fake@mail.com"
+      fill_in "Description", with: "CHANGE FAKE"
+      click_on "Update Resource"
+      expect(current_path).to eq(organization_path(organization))
+      expect(page).to have_text("Organization Name")
+    end
+  end
 end

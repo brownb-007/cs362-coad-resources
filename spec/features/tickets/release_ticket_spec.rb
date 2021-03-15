447db93867db639ef6c9878823c66bcf03028d6d
Brayden Brown <braydenbrown@gmail.com>
2021-03-14 17:24:43 -0700

    Ticket: system test for an organization releasing a ticket

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe 'Releasing a ticket by an', type: :feature do
+RSpec.describe 'Releasing a ticket', type: :feature do
+  let(:organization) { create(:organization, :approved) }
+  let(:user) { create(:user, :mem_of_org, organization: organization) }
+  let(:ticket) { create(:ticket) }
+  let(:admin) { admin = create(:user, :admin) }
 
+  specify "Organization releases a ticket" do
+    log_in_as(user)
+    visit ticket_path(ticket.id)
+    click_on "Capture"
+    expect(current_path).to eq(dashboard_path)
+    visit ticket_path(ticket.id)
+    click_on "Release"
+    expect(current_path).to eq(dashboard_path)
+    visit ticket_path(ticket.id)
+    expect(page).to have_text("Captured by\n")
+  end
 end

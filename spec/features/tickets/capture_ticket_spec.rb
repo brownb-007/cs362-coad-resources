bf80a2b5f03a1ea5bd1d4173cf363e013217b41f
Brayden Brown <braydenbrown@gmail.com>
2021-03-14 17:20:52 -0700

    Ticket: create a system test for an organization capturing a ticket and that a captured ticket cant be recaptured

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
+  let(:organization) { create(:organization, :approved) }
+  let(:user) { create(:user, :mem_of_org, organization: organization) }
+  let(:ticket) { create(:ticket) }
 
+  specify "Organization captures a ticket" do
+    log_in_as(user)
+    visit ticket_path(ticket.id)
+    click_on "Capture"
+    expect(current_path).to eq(dashboard_path)
+    visit ticket_path(ticket.id)
+    expect(page).to have_text("Captured by #{organization}")
+  end
+
+  specify "Organization cant capture a captured ticket" do
+    log_in_as(user)
+    visit ticket_path(ticket.id)
+    click_on "Capture"
+    expect(current_path).to eq(dashboard_path)
+    visit ticket_path(ticket.id)
+    expect(page).to have_text("Captured by #{organization}")
+    visit ticket_path(ticket.id)
+    expect(page).to_not have_link("Capture")
+  end
 end

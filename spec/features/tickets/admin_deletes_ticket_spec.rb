ba30cf0a4b316a061b127f16cd882862c97803b1
Brayden Brown <braydenbrown@gmail.com>
2021-03-14 17:17:27 -0700

    Tickets: create a system test for an admin deleting a ticket in the system

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
-
+  let(:admin) { admin = create(:user, :admin) }
+  let(:ticket) { create(:ticket) }
+  
+  specify "Admin deletes a ticket" do
+    log_in_as(admin)
+    visit ticket_path(ticket.id)
+    click_on "Delete"
+    expect(page).to have_text("Ticket #{ticket.id} was deleted")
+  end
 end
